8c5f1c962a7d49432d2d59f208dcf7780dbb6394
AMOL NAGRALE <amolmnagrale1@gmail.com>
2021-06-27 18:11:03 +0530

    Practice set

--- Practice_set/Multi threading.py
+++
@@ -1,9 +0,0 @@
-# Multi threading
-from threading import*
-def display():
-    for i in range(1,11):
-        print("Child Tread")
-t = Thread(target = display)
-t.start()
-for i in range(1,11):
-    print("Main Thread")    # by default 
